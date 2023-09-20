6191824edb38e4c1e1e196f0f8e32485010ab4ac
Emik Adiani <emikadiani94@gmail.com>
2023-09-20 10:58:16 +0800

    add test cases for feature: login

---
+++ Scripts/Login/block-signup-success/Script1695177009736.groovy
@@ -0,0 +1,51 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser(GlobalVariable.baseUrl)
+
+WebUI.setViewPortSize(GlobalVariable.viewportWidth, GlobalVariable.viewportHeight)
+
+WebUI.waitForElementVisible(findTestObject('HomePage/h1_PRODUCT STORE'), 0)
+
+WebUI.verifyElementVisible(findTestObject('HomePage/h1_PRODUCT STORE'), FailureHandling.CONTINUE_ON_FAILURE)
+
+WebUI.click(findTestObject('HomePage/menu_Sign up'))
+
+WebUI.waitForElementVisible(findTestObject('SignUp-Page/h2_Sign up'), 0)
+
+WebUI.verifyElementVisible(findTestObject('SignUp-Page/h2_Sign up'))
+
+WebUI.setText(findTestObject('SignUp-Page/txt_signup_Username'), GlobalVariable.validUsername)
+
+WebUI.setText(findTestObject('SignUp-Page/txt_signup_Password'), GlobalVariable.validPassword)
+
+WebUI.click(findTestObject('SignUp-Page/btn_Sign up'))
+
+WebUI.waitForAlert(2)
+
+alertSignUp1 = WebUI.getAlertText()
+
+println('alert text is: ' + alertSignUp1)
+
+WebUI.verifyMatch(alertSignUp1, 'Sign up successful.', false)
+
+WebUI.verifyAlertPresent(2)
+
+WebUI.acceptAlert()
+
